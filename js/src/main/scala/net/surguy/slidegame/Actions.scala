c65410d5b140db42e65b0eb06c01de3f4d075154
Inigo <inigo.surguy@gmail.com>
2017-08-07 21:04:16 +0100

    Simplify observers by using partial functions

--- js/src/main/scala/net/surguy/slidegame/Actions.scala
+++ js/src/main/scala/net/surguy/slidegame/Actions.scala
@@ -3,16 +3,13 @@ package net.surguy.slidegame
 import net.surguy.slidegame.shared.{BoardState, Direction, GameSetup, Piece}
 
 class Observable() {
-  private var observers: List[Observer[_]] = List()
-  def registerObserver[T <: Message](observer: Observer[T]): Unit = { observers = observer :: observers }
-  def notifyObservers[T <: Message](message: T): Unit = {
-    observers.filter(_.isAppropriateFor(message)).foreach(o => o.asInstanceOf[Observer[T]].notify(message))
-  }
-}
+  type Observer = PartialFunction[Message, Unit]
 
-abstract class Observer[T <: Message] {
-  def isAppropriateFor(msg: Message): Boolean
-  def notify(msg: T)
+  private var observers: List[Observer] = List()
+  def registerObserver[T <: Message](observer: Observer): Unit = { observers = observer :: observers }
+  def notifyObservers[T <: Message](message: T): Unit = {
+    observers.filter(_.isDefinedAt(message)).foreach(o => o(message))
+  }
 }
 
 sealed trait Message
@@ -26,47 +23,29 @@ case class TimeTravel(isBackward: Boolean)
   * Actions that change the state of the game.
   */
 class GameActions(private var boardState: BoardState, observable: Observable, displayFn: (BoardState, Observable) => Unit) {
-
-  class SetActiveObserver extends Observer[SetActive] {
-    override def isAppropriateFor(msg: Message): Boolean = msg.isInstanceOf[SetActive]
-    override def notify(msg: SetActive): Unit = {
-      val newState = boardState.setActive(msg.piece.name)
+  val observers: Seq[PartialFunction[Message, Unit]] = List(
+    { case SetActive(piece) =>
+      val newState = boardState.setActive(piece.name)
       observable.notifyObservers(UpdateBoard(newState))
-    }
-  }
-
-  class MoveActiveObserver extends Observer[MoveActive] {
-    override def isAppropriateFor(msg: Message): Boolean = msg.isInstanceOf[MoveActive]
-    override def notify(msg: MoveActive): Unit = {
-      boardState.moveActivePiece(msg.direction) match {
+    },
+    { case MoveActive(direction) =>
+      boardState.moveActivePiece(direction) match {
         case Some(newState) => observable.notifyObservers(UpdateBoard(newState))
         case None => // Invalid mode - do nothing
       }
-    }
-  }
-
-  class ResetObserver extends Observer[Reset] {
-    override def isAppropriateFor(msg: Message): Boolean = msg.isInstanceOf[Reset]
-    override def notify(msg: Reset): Unit = {
-      GameSetup.initialStates.find(_.name==msg.stateName) match {
-        case Some(resetState) => observable.notifyObservers(UpdateBoard(resetState))
-        case None => throw new IllegalStateException("Attempting to reset state to unknown state "+msg.stateName)
-      }
-    }
-  }
-
-  class UpdateBoardObserver extends Observer[UpdateBoard] {
-    override def isAppropriateFor(msg: Message): Boolean = msg.isInstanceOf[UpdateBoard]
-    override def notify(msg: UpdateBoard): Unit = {
-      boardState = msg.newBoard
+    },
+    { case Reset(stateName) =>
+       GameSetup.initialStates.find(_.name==stateName) match {
+         case Some(resetState) => observable.notifyObservers(UpdateBoard(resetState))
+         case None => throw new IllegalStateException("Attempting to reset state to unknown state "+stateName)
+       }
+    },
+    { case UpdateBoard(newBoard) =>
+      boardState = newBoard
       displayFn(boardState, observable)
-    }
-  }
+    },
+  )
 
-  observable.registerObserver(new SetActiveObserver())
-  observable.registerObserver(new MoveActiveObserver())
-  observable.registerObserver(new ResetObserver())
-  observable.registerObserver(new UpdateBoardObserver())
-
+  observers.foreach(observable.registerObserver)
   observable.notifyObservers(UpdateBoard(boardState))
 }
